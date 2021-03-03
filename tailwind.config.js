add4e86471414ae6bc6fa5915135cfaea47d9149
ayumitanaka <tanakayumi13@gmail.com>
2021-03-03 18:05:10 +0000

    modified home.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        beige: '#f5d7b2',
+      },
       fontFamily: {
         aurore: ['La Belle Aurore', 'cursive'],
         josefin: ['Josefin Sans', 'sans-serif'],
