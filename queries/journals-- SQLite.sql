341a90d37fe961b97fac486e4e71e3d43e8af657
ayumitanaka <tanakayumi13@gmail.com>
2021-03-03 18:42:12 +0000

    modified queries

--- queries/journals-- SQLite.sql
+++ queries/journals-- SQLite.sql
@@ -2,7 +2,7 @@
 
 DELETE FROM journals;
 
-INSERT INTO artists (id, year, name, name_j, title, title_j, youtube, song_info, artist_info, song_picture_path, artist_picture_path, from_album_id)
+INSERT INTO journals (from_user_id, start_date, end_date, country, city, title, comment, picture_path)
 VALUES (1, 1982, 'Eiichi Ohtaki', '大瀧 詠一', 'Kimi wa Tennen Shoku', '君は天然色', 'https://www.youtube.com/embed/dH9yLGoIxBw',
 2020/12/15 2020/12/18 Canada Vancouver Bright days, Vancouver! 
 I went to the lighthouse park in Vancouver. Awesome park with great viewpoints on the different spots. If you love spending time out at the seashore and love hiking, watching the sun rises and sets. I swear this place is the best to visit for you. I had a great time out there enjoying the decent views and got a chance to see the beautiful sunset view there✌🏻
